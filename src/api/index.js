cad0f7392164eb7ceb95242361f77b79a2d8918a
chunmuyu <3232104757@qq.com>
2023-02-04 15:47:10 +0800

    支付界面设计

--- src/api/index.js
+++ src/api/index.js
@@ -84,3 +84,19 @@ export const reqOrderInfo = ()=>requests({
     url:`/order/auth/trade`,
     method:'get'
 })
+
+export const reqSubmitOrder = (tradeNo,data)=>requests({
+    url:`/order/auth/submitOrder?tradeNo=${tradeNo}`,
+    data,
+    method:'post'
+})
+
+export const reqPayInfo = (orderId)=>requests({
+    url:`/payment/weixin/createNative/${orderId}`,
+    method:'get'
+})
+
+export const reqPayStatus = (orderId)=>requests({
+    url:`/payment/weixin/queryPayStatus/${orderId}`,
+    method:'get'
+})
--- src/main.js
+++ src/main.js
@@ -9,8 +9,13 @@ import store from '@/store'
 
 import '@/mock/mockServe'
 import 'swiper/css/swiper.css'
+import * as API from '@/api'
+import { Button ,MessageBox} from 'element-ui';
 
+Vue.prototype.$msgbox = MessageBox
+Vue.prototype.$alert = MessageBox.alert
 
+Vue.component(Button.name, Button);
 Vue.component(TypeNav.name,TypeNav)
 Vue.component(Carousel.name,Carousel)
 Vue.component(Pagination.name,Pagination)
@@ -24,6 +29,7 @@ new Vue({
   render: h => h(App),
   beforeCreate() {
     Vue.prototype.$bus=this
+    Vue.prototype.$API=API
   },
   router,
   store
